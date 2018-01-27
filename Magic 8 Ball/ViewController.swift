e6a9bfb45ca0c18b1f6b00d66b587f6afa509e05
davidbrianporter@gmail.com <davidbrianporter@gmail.com>
2018-01-27 08:00:17 -0800

    first commit of xcode project, filling an empty repository

---
+++ Magic 8 Ball/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Magic 8 Ball
+//
+//  Created by Captain on 1/27/18.
+//  Copyright © 2018 Captain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
